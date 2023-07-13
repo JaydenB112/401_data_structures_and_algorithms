b413ef85e9e92202b0e0240881085996d87284e8
Jayden Boyd <jaydenboydb1@gmail.com>
2023-07-13 13:36:40 -0500

    Ch 1 is Passing

--- c-sharp/CodeChallenge1/ArrayReverse.cs
+++ c-sharp/CodeChallenge1/ArrayReverse.cs
@@ -1,12 +1,20 @@
 using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+
 namespace CodeChallenges
 {
-    public class CodeChallenge1
+  public class CodeChallenge1
+  {
+    public static int[] ReverseArray(int[] array)
     {
-        public static int[] ReverseArray(int[] array)
-        {
-
-            return array;
-        }
+      int[] tempArray = new int[array.Length];
+      int taLength = tempArray.Length;
+      for (int i = array.Length; i > 0; i--)
+      {
+        tempArray[taLength - i] = array[i - 1];
+        //Console.WriteLine(array[i]);
+      }
+      return tempArray;
     }
+  }
 }
