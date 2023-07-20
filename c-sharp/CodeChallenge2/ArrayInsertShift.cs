6ed77a225de1e92f18b4ca4b137ff27a3f060446
Jayden Boyd <jaydenboydb1@gmail.com>
2023-07-20 10:37:33 -0500

    Lab 02 3/4

--- c-sharp/CodeChallenge2/ArrayInsertShift.cs
+++ c-sharp/CodeChallenge2/ArrayInsertShift.cs
@@ -1,11 +1,24 @@
-namespace CodeChallenges 
+namespace CodeChallenges
 {
-    public class CodeChallenge2
+  public class CodeChallenge2
+  {
+    public static int[] InsertShiftArray(int[] array, int value)
     {
-        public static int[] InsertShiftArray(int[] array, int value)
-        {
-            //insert the value into the middle of the array
-            return array;
-        }
+      int midindex = array.Length / 2;
+      int[] newArray = new int[array.Length + 1];
+      for (int i = 0; i < midindex; i++)
+      {
+        newArray[i] = array[i];
+      }
+      newArray[midindex] = value;
+      for (int i = midindex + 1; i < newArray.Length; i++)
+      {
+        newArray[i] = array[i - 1];
+      }
+        return newArray;
+      
+
+      
     }
+  }
 }
