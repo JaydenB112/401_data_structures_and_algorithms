a1e83b4dcf88c0f387cab7f1e1750fb966b8abf4
Jayden Boyd <jaydenboydb1@gmail.com>
2023-07-26 13:19:16 -0500

    Almost Done!

--- c-sharp/CodeChallenge3/ArrayBinarySearch.cs
+++ c-sharp/CodeChallenge3/ArrayBinarySearch.cs
@@ -1,9 +1,28 @@
+using System.ComponentModel.Design;
+
 namespace CodeChallenges 
 {
     public class CodeChallenge3
     {
         public static int BinarySearch(int[] array, int key)
         {
+        int high = array.Length - 1;
+        int low = 0;
+
+        while(low <= high)
+      {
+        int mid = low +(high - low) / 2 ;
+        if (array[mid] == key)
+        {
+          while(mid > 0 && array[mid - 1] == key)
+          {
+            mid--;
+          }return mid;
+          { 
+         else if  
+          }
+        }
+      }
             int foundElementIndex = -1;
             return foundElementIndex;
         }
